12d0ec0a06a69f77222ac76e415147d844e9bdba
Ivan Malyshev <ivan.malyshevv@gmail.com>
2026-04-19 19:04:24 +0300

    fix: Исправлены замечания по праверке прав и импортов

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,36 +1,30 @@
 from rest_framework import viewsets
 from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
 from posts.models import Post, Group, Comment
 from .serializers import PostSerializer, GroupSerializer, CommentSerializer
+from .permissions import IsAuthorOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
-
-    def perform_update(self, serializer):
-        if serializer.instance.author != self.request.user:
-            from rest_framework.exceptions import PermissionDenied
-            raise PermissionDenied('Изменение чужого контента запрещено!')
-        serializer.save()
-
-    def perform_destroy(self, instance):
-        if instance.author != self.request.user:
-            from rest_framework.exceptions import PermissionDenied
-            raise PermissionDenied('Удаление чужого контента запрещено!')
-        instance.delete()
 
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
 
     def get_queryset(self):
         post_id = self.kwargs.get('post_id')
@@ -40,15 +34,3 @@ class CommentViewSet(viewsets.ModelViewSet):
         post_id = self.kwargs.get('post_id')
         post = get_object_or_404(Post, id=post_id)
         serializer.save(author=self.request.user, post=post)
-
-    def perform_update(self, serializer):
-        if serializer.instance.author != self.request.user:
-            from rest_framework.exceptions import PermissionDenied
-            raise PermissionDenied('Изменение чужого контента запрещено!')
-        serializer.save()
-
-    def perform_destroy(self, instance):
-        if instance.author != self.request.user:
-            from rest_framework.exceptions import PermissionDenied
-            raise PermissionDenied('Удаление чужого контента запрещено!')
-        instance.delete()
